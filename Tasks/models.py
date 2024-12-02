a64caf3488f4a965dcdc567776a2ce4b2e3aa1cd
HidanCodes <HidanCodes@gmail.com>
2024-12-02 13:01:55 +0330

    migration 2
    adding start and end date

--- Tasks/models.py
+++ Tasks/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True,null=True)
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateField(blank=True,null=True)
+    end_date = models.DateField(blank=True, null=True)
 
 
     def __str__(self):
