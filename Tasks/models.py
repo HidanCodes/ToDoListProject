83bd976001cd538cf15f99fa8321f9fa90eb4f30
HidanCodes <HidanCodes@gmail.com>
2024-12-02 12:48:24 +0330

    migration
    first migration in db

--- Tasks/models.py
+++ Tasks/models.py
@@ -7,5 +7,6 @@ class Task(models.Model):
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return self.title
