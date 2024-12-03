062d835285a36161b9db0f767feacc4b6dbcc25c
HidanCodes <HidanCodes@gmail.com>
2024-12-03 15:26:09 +0330

    add Serializer

--- ToDoListProject/settings.py
+++ ToDoListProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Tasks.apps.TasksConfig"
+    "Tasks.apps.TasksConfig",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
