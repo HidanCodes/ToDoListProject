61f7559ccf2a83c263477a2fb9cca7c7c43c57c1
HidanCodes <HidanCodes@gmail.com>
2024-12-03 15:18:37 +0330

    adding category
    category added and migrated

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category (models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True,null=True)
@@ -8,6 +13,7 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     start_date = models.DateField(blank=True,null=True)
     end_date = models.DateField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
     def __str__(self):
